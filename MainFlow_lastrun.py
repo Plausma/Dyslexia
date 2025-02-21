356a17070e866bad106fe4d9b90371a04dba7098
Plausma <82110877+Plausma@users.noreply.github.com>
2025-02-21 12:54:11 +0700

    add necessary file for an actual testing
    add image, text image, and update the imageid.xlsx to let the psychopy choose the image

--- MainFlow_lastrun.py
+++ MainFlow_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.1),
-    on February 21, 2025, at 00:23
+    on February 21, 2025, at 12:50
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -373,23 +373,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
     text_norm.alignText= 'center'
     key_instruct = keyboard.Keyboard(deviceName='key_instruct')
     
-    # --- Initialize components for Routine "demo_img" ---
-    image_3 = visual.ImageStim(
-        win=win,
-        name='image_3', units='norm', 
-        image='demo/tutorimg.png', mask=None, anchor='center',
-        ori=0.0, pos=(0, 0.3), size=(None, 1),
-        color=[1,1,1], colorSpace='rgb', opacity=None,
-        flipHoriz=False, flipVert=False,
-        texRes=128.0, interpolate=True, depth=0.0)
-    text_4 = visual.TextStim(win=win, name='text_4',
-        text='As explained before, the image will be shown for a certain period of time, then, two words will appear. Click on the word that best explain the image\n\npress spacebar to start the experiment',
-        font='Open Sans',
-        units='norm', pos=(0, -0.5), height=0.1, wrapWidth=None, ori=0.0, 
-        color='white', colorSpace='rgb', opacity=None, 
-        languageStyle='LTR',
-        depth=-1.0);
-    
     # --- Initialize components for Routine "blank1sec" ---
     
     # --- Initialize components for Routine "imgshow" ---
@@ -582,118 +565,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         thisExp.addData('key_instruct.duration', key_instruct.duration)
     thisExp.nextEntry()
     # the Routine "tutorial" was not non-slip safe, so reset the non-slip timer
-    routineTimer.reset()
-    
-    # --- Prepare to start Routine "demo_img" ---
-    continueRoutine = True
-    # update component parameters for each repeat
-    thisExp.addData('demo_img.started', globalClock.getTime(format='float'))
-    # keep track of which components have finished
-    demo_imgComponents = [image_3, text_4]
-    for thisComponent in demo_imgComponents:
-        thisComponent.tStart = None
-        thisComponent.tStop = None
-        thisComponent.tStartRefresh = None
-        thisComponent.tStopRefresh = None
-        if hasattr(thisComponent, 'status'):
-            thisComponent.status = NOT_STARTED
-    # reset timers
-    t = 0
-    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-    frameN = -1
-    
-    # --- Run Routine "demo_img" ---
-    routineForceEnded = not continueRoutine
-    while continueRoutine:
-        # get current time
-        t = routineTimer.getTime()
-        tThisFlip = win.getFutureFlipTime(clock=routineTimer)
-        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-        # update/draw components on each frame
-        
-        # *image_3* updates
-        
-        # if image_3 is starting this frame...
-        if image_3.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-            # keep track of start time/frame for later
-            image_3.frameNStart = frameN  # exact frame index
-            image_3.tStart = t  # local t and not account for scr refresh
-            image_3.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(image_3, 'tStartRefresh')  # time at next scr refresh
-            # add timestamp to datafile
-            thisExp.timestampOnFlip(win, 'image_3.started')
-            # update status
-            image_3.status = STARTED
-            image_3.setAutoDraw(True)
-        
-        # if image_3 is active this frame...
-        if image_3.status == STARTED:
-            # update params
-            pass
-        
-        # *text_4* updates
-        
-        # if text_4 is starting this frame...
-        if text_4.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-            # keep track of start time/frame for later
-            text_4.frameNStart = frameN  # exact frame index
-            text_4.tStart = t  # local t and not account for scr refresh
-            text_4.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(text_4, 'tStartRefresh')  # time at next scr refresh
-            # add timestamp to datafile
-            thisExp.timestampOnFlip(win, 'text_4.started')
-            # update status
-            text_4.status = STARTED
-            text_4.setAutoDraw(True)
-        
-        # if text_4 is active this frame...
-        if text_4.status == STARTED:
-            # update params
-            pass
-        
-        # if text_4 is stopping this frame...
-        if text_4.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > text_4.tStartRefresh + 1.0-frameTolerance:
-                # keep track of stop time/frame for later
-                text_4.tStop = t  # not accounting for scr refresh
-                text_4.tStopRefresh = tThisFlipGlobal  # on global time
-                text_4.frameNStop = frameN  # exact frame index
-                # add timestamp to datafile
-                thisExp.timestampOnFlip(win, 'text_4.stopped')
-                # update status
-                text_4.status = FINISHED
-                text_4.setAutoDraw(False)
-        
-        # check for quit (typically the Esc key)
-        if defaultKeyboard.getKeys(keyList=["escape"]):
-            thisExp.status = FINISHED
-        if thisExp.status == FINISHED or endExpNow:
-            endExperiment(thisExp, win=win)
-            return
-        
-        # check if all components have finished
-        if not continueRoutine:  # a component has requested a forced-end of Routine
-            routineForceEnded = True
-            break
-        continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in demo_imgComponents:
-            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                continueRoutine = True
-                break  # at least one component has not yet finished
-        
-        # refresh the screen
-        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-            win.flip()
-    
-    # --- Ending Routine "demo_img" ---
-    for thisComponent in demo_imgComponents:
-        if hasattr(thisComponent, "setAutoDraw"):
-            thisComponent.setAutoDraw(False)
-    thisExp.addData('demo_img.stopped', globalClock.getTime(format='float'))
-    thisExp.nextEntry()
-    # the Routine "demo_img" was not non-slip safe, so reset the non-slip timer
     routineTimer.reset()
     
     # set up handler to look after randomisation of conditions etc
