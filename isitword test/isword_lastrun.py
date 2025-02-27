009c1f39cc623cb8cb6698a1b5457ba6d9e726a8
Plausma <82110877+Plausma@users.noreply.github.com>
2025-02-28 00:31:39 +0700

    recreate analysis file for the plot

--- isitword test/isword_lastrun.py
+++ isitword test/isword_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.1),
-    on February 27, 2025, at 12:10
+    on February 27, 2025, at 20:53
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
